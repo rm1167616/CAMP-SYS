c7ecb72285825785ee8a80edbf47671516685c49
toqa osama <82851804+toqaosama@users.noreply.github.com>
2024-10-06 14:41:41 +0300

    40%

--- CLIENT/src/pages/shared/header/header.js
+++ CLIENT/src/pages/shared/header/header.js
@@ -6,30 +6,30 @@ const Header = () => {
   return (
     <header>
       <Container fluid>
-        <Nav className="justify-content-center bg-success py-3">
+        <Nav className="justify-content-center bg-dark py-3" >
           <Nav.Item>
-            <Nav.Link href="#stay" className="text-light">Stay</Nav.Link>
+            <Nav.Link href="#stay" className="text-warning">Stay</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#dine" className="text-light">Dine</Nav.Link>
+            <Nav.Link href="#dine" className="text-warning">Dine</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#weddings" className="text-light">Weddings</Nav.Link>
+            <Nav.Link href="#weddings" className="text-warning">Weddings</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#meetings" className="text-light">Meetings</Nav.Link>
+            <Nav.Link href="#meetings" className="text-warning">Meetings</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#celebrations" className="text-light">Celebrations</Nav.Link>
+            <Nav.Link href="#celebrations" className="text-warning">Celebrations</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#christmas" className="text-light">Christmas 2024</Nav.Link>
+            <Nav.Link href="#christmas" className="text-warning">Christmas 2024</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#events" className="text-light">Special Events</Nav.Link>
+            <Nav.Link href="#events" className="text-warning">Special Events</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#contact" className="text-light">Contact</Nav.Link>
+            <Nav.Link href="#contact" className="text-warning">Contact</Nav.Link>
           </Nav.Item>
         </Nav>
       </Container>
