8b2c6d63b7a7b37fd97d2a0cd45f97f8dc656e22
TarekTawfiq <165324565+tarektawfiq@users.noreply.github.com>
2024-10-08 16:04:15 +0300

    SomeEditesInFooter

--- CLIENT/src/pages/shared/footer/footer.js
+++ CLIENT/src/pages/shared/footer/footer.js
@@ -2,30 +2,43 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/footer.css'; // Custom CSS for styling
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
-    <footer className="footer bg-dark text-light py-4">
-      <Container>
+    <footer className="footer bg-dark text-light">
+      <Container className='ConatainreFooter py-3'>
         <Row className="justify-content-between">
-          <Col xs={12} md={4} className="text-center text-md-left mb-3">
-            <img src="/path-to-logo" alt="Camp Sys Logo" className="footer-logo mb-3" />
-            <p>&copy; 2024 Camp Sys. All Rights Reserved.</p>
+          <Col xs={12} md={4} className="text-center text-md-left ">
+            <div className='ConLogo'>
+              <img src="https://i.pinimg.com/736x/48/b0/87/48b087b50ead6993a8113c440d640db4.jpg" alt="Camp Sys Logo" className="footer-logo" />
+              <p>WebBugs</p>
+            </div>
+
           </Col>
-          <Col xs={12} md={4} className="text-center mb-3">
-            <ul className="list-unstyled">
-              <li>Contact</li>
-              <li>T & Cs</li>
-              <li>Cookie Policy</li>
-              <li>Privacy Policy</li>
-              <li>Gift Vouchers</li>
-              <li>Careers</li>
-            </ul>
+          <Col xs={12} md={4} className="text-center">
+            <div className="list-unstyled list-unstyledOptions">
+              <a href='#'>Contact</a>
+              <a href='#'>T & Cs</a>
+              <a href='#'>Cookie Policy</a>
+              <a href='#'>Privacy Policy</a>
+              <a href='#'>Gift Vouchers</a>
+              <a href='#'>Careers</a>
+            </div>
           </Col>
-          <Col xs={12} md={4} className="text-center text-md-right mb-3">
-            <p>02084618600</p>
-            <p>enquiries@campsys.com</p>
-            <ul className="list-inline">
+          <Col xs={12} md={4} className="text-center text-md-right ParentContactUs">
+            <div className='ContactUs'>
+              <p>02084618600</p>
+              <p>enquiries@campsys.com</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <div className='EndFooter'>
+        <div className="ConEndFooter">
+        <p>&copy; 2024 Camp Sys. Creat With 🤍 By <span className='Webbugs'>WebBugs</span></p>
+                    <ul className="list-inline">
               <li className="list-inline-item">
                 <a href="#facebook" className="social-icon">
                   <i className="fab fa-facebook"></i>
@@ -52,9 +65,8 @@ const Footer = () => {
                 </a>
               </li>
             </ul>
-          </Col>
-        </Row>
-      </Container>
+        </div>
+      </div>
     </footer>
   );
 };
