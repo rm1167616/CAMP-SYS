8a73f468bf3b1a6430868f7e10861c0b8608b4a4
RaDwan <119738657+rm1167616@users.noreply.github.com>
2024-10-09 13:09:03 +0300

    rr

---
+++ CLIENT/src/pages/routes/routes.js
@@ -0,0 +1,56 @@
+import { createBrowserRouter, Navigate} from "react-router-dom";
+import Home from '../pages/home/home';
+import Login from '../pages/Authh/Login';
+import Register from '../pages/Authh/Register';
+import App from "../App";
+import AddOffer from "../pages/offers/AddOffer";
+import UpdateOffer from "../pages/offers/UpdateOffer";
+import Offers from "../pages/offers/Offers";
+import Contact from "../pages/Contact/Contact";
+
+//
+export const routes = createBrowserRouter([
+{ 
+    path : '', 
+    element: <App/> , // Parent 
+    children: [
+        {
+            path: "/",
+            element: <Home />,
+        },
+        {
+            path: "/login",
+            element: <Login />,
+        },
+        {
+            path: "/register",
+            element: <Register />,
+        },    
+        {
+            path: "/offers",
+            children: [ // no '/' in children to avoid errors 
+            {
+                path: '' ,
+                element: <Offers/>,
+            },
+            {
+                path: 'add' , // no '/' before add
+                element: <AddOffer/>,
+            },
+            {
+                path: ':id' ,
+                element: <UpdateOffer/> ,
+            },]
+        },   
+        {
+            path: "/contact",
+            element: <Contact/>,
+        },  
+
+]
+},
+        {
+            path: "*", // in case of wrong url, go to home  
+            element: <Navigate to={"/"} />,
+        },
+]);
