db9ac08774a43e55f1addbb4bd4e556478305464
RaDwan <119738657+rm1167616@users.noreply.github.com>
2024-10-19 12:51:01 +0300

    delete routes

--- CLIENT/src/routes/routes.js
+++ CLIENT/src/routes/routes.js
@@ -55,3 +55,77 @@ export const routes = createBrowserRouter([
             element: <Navigate to={"/"} />,
         },
 ]);
+
+
+
+
+/*
+import { createBrowserRouter, Navigate} from "react-router-dom";
+import Home from '../pages/home/home';
+import Login from '../pages/Authh/Login';
+import Register from '../pages/Authh/Register';
+import App from "../App";
+import Offers from "../pages/offers/Offers" ;
+import Contact from "../pages/Contact/Contact";
+import Benfite from "../homeused/benfite"
+import Cards from "../homeused/cards"
+import RoomFeatures from "../homeused/roomfeature"
+import AboutRooms from "../homeused/AboutRooms"
+import Rooms from "../Rooms/room"
+
+
+//
+export const routes = createBrowserRouter([
+{ 
+    path : '', 
+    element: <App/> , // Parent 
+    children: [
+        {
+            path: "/",
+            element: <Home />,
+        },
+        {
+            path: "/login",
+            element: <Login />,
+        },
+        {
+            path: "/register",
+            element: <Register />,
+        },  
+        {
+            path: "/benfite",
+            element: <Benfite />,
+        },
+        {
+            path: "/cards",
+            element: <Cards />,
+        },   
+        {
+            path: "/roomfeatures",
+            element: <RoomFeatures />,
+        },  
+        {
+            path: "/AboutRooms",
+            element: <AboutRooms />,
+        },  
+        {
+            path: "/offers",
+            element: <Offers />,
+        },   
+        {
+            path: "/contact",
+            element: <Contact/>,
+        },  
+        {
+            path: "/Rooms",
+            element: <Rooms/>,
+        }, 
+
+]
+},
+        {
+            path: "*", // in case of wrong url, go to home  
+            element: <Navigate to={"/"} />,
+        },
+]);
+*/
